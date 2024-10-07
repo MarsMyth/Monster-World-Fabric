5f64777bf04c10424e68041b947ff48a092cc14c
MarsMyth <callumshelton2503@gmail.com>
2024-10-07 17:00:43 +1000

    Launch Commit

---
+++ src/main/java/net/mars_myth/monsterworld/MoonyMonsterWorldDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mars_myth.monsterworld;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoonyMonsterWorldDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
