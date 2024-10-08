a07d853e4a59ba98538dd3a67b8a1f0936ace70e
MarsMyth <callumshelton2503@gmail.com>
2024-10-08 18:25:48 +1000

    Basic Ball is now Throwable

--- src/main/java/net/mars_myth/monsterworld/item/balls/custom/BasicMonsterBall.java
+++ src/main/java/net/mars_myth/monsterworld/item/balls/custom/BasicMonsterBall.java
@@ -1,16 +1,19 @@
 package net.mars_myth.monsterworld.item.balls.custom;
 
-import net.mars_myth.monsterworld.entity.ball.BasicBallProjectileEntity;
+
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.thrown.SnowballEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-
 import net.minecraft.world.World;
+
 
 public class BasicMonsterBall extends Item {
     public BasicMonsterBall(Settings settings) {
@@ -19,21 +22,28 @@ public class BasicMonsterBall extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand);
-        world.playSound(null, user.getX(), user.getY(), user.getZ(),
-                SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
 
+        ItemStack itemStack = user.getStackInHand(hand);
+        world.playSound(
+                null,
+                user.getX(),
+                user.getY(),
+                user.getZ(),
+                SoundEvents.ENTITY_SNOWBALL_THROW,
+                SoundCategory.NEUTRAL,
+                0.5F,
+                0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F)
+        );
         if (!world.isClient) {
-            BasicBallProjectileEntity basicBallProjectileEntity = new BasicBallProjectileEntity(user, world);
-            basicBallProjectileEntity.setItem(itemStack);
-            basicBallProjectileEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0f, 1.5f, 1.0f);
-            world.spawnEntity(basicBallProjectileEntity);
+            SnowballEntity snowballEntity = new SnowballEntity(world, user);
+            snowballEntity.setItem(itemStack);
+            snowballEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
+            world.spawnEntity(snowballEntity);
         }
 
         user.incrementStat(Stats.USED.getOrCreateStat(this));
-        if (!user.getAbilities().creativeMode) {
-            itemStack.decrement(1);
-        }
-        return TypedActionResult.success(itemStack, world.isClient());
+        itemStack.decrementUnlessCreative(1, user);
+
+        return super.use(world, user, hand);
     }
 }
